3d23e662f7b067337d3357761d727dfd7b6d230b
Paulo Roberto <paulo.pinda@gmail.com>
2024-04-02 11:00:35 -0300

    Fixes

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,16 +3,9 @@ local vim = vim
 vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", { silent = true })
 vim.api.nvim_set_keymap("n", "<C-d>", ":TagbarToggle<CR>", { silent = true })
 vim.api.nvim_set_keymap("n", "<C-t>", ":Twilight<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-s>", ":Lspsaga", { silent = true })
 vim.api.nvim_set_keymap("n", "<C-l>", ":TroubleToggle<CR>", { silent = true })
 
 vim.api.nvim_set_keymap("n", "<C-down>", "<C-down> <C-W><C-J>", { silent = true })
 vim.api.nvim_set_keymap("n", "<C-up>", "<C-W><C-K>", { silent = true })
 vim.api.nvim_set_keymap("n", "<C-right>", "<C-W><C-L>", { silent = true })
 vim.api.nvim_set_keymap("n", "<C-left>", "<C-W><C-H>", { silent = true })
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-    callback = function()
-      require("lint").try_lint()
-    end,
-})
